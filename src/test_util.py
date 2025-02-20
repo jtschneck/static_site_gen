a3389755a2965e6147369dc965cdd86713dee0ed
jtschneck <jtschneck@gmail.com>
2025-02-20 14:33:12 -0800

    Add image and link node extract

--- src/test_util.py
+++ src/test_util.py
@@ -40,6 +40,70 @@ class TestTextNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             split_nodes_delimiter([node], "`", TextType.CODE)
 
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected = [
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ]
+        self.assertEqual(expected, extract_markdown_images(text))
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        expected = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev"),
+        ]
+        self.assertEqual(expected, extract_markdown_links(text))
+
+    def test_split_nodes_link_two_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        res = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+             ),
+        ]
+        self.assertEqual(res, new_nodes)
+
+    def test_split_nodes_image_two_images(self):
+        node = TextNode(
+            "This is text with an image ![of boots](boots.png) and ![of youtube](youtube.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        res = [
+            TextNode("This is text with an image ", TextType.TEXT),
+            TextNode("of boots", TextType.IMAGE, "boots.png"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode(
+                "of youtube", TextType.IMAGE, "youtube.png"
+             ),
+        ]
+        self.assertEqual(res, new_nodes)
+
+    def test_split_nodes_image_link(self):
+        node = TextNode(
+            "This is text with an image ![of boots](boots.png) and a link [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        new_nodes = split_nodes_link(new_nodes)
+        res = [
+            TextNode("This is text with an image ", TextType.TEXT),
+            TextNode("of boots", TextType.IMAGE, "boots.png"),
+            TextNode(" and a link ", TextType.TEXT),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+             ),
+        ]
+        self.assertEqual(res, new_nodes)
 
 
 
--- src/util.py
+++ src/util.py
@@ -1,3 +1,4 @@
+import re
 from textnode import *
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -36,3 +37,56 @@ def split_single_text_node(text, delimiter, text_type):
         ret.extend(split_single_text_node(split_text[1], delimiter, text_type))
 
     return ret
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_link(old_nodes):
+    ret = []
+    for old_node in old_nodes:
+        if old_node.text_type == TextType.TEXT:
+            link_matches = extract_markdown_links(old_node.text)
+            if (link_matches):
+                ret.extend(split_node_matches_helper(old_node.text, link_matches, TextType.LINK))
+                continue
+        ret.append(old_node)
+    return ret
+
+def split_nodes_image(old_nodes):
+    ret = []
+    for old_node in old_nodes:
+        if old_node.text_type == TextType.TEXT:
+            image_matches = extract_markdown_images(old_node.text)
+            if (image_matches):
+                ret.extend(split_node_matches_helper(old_node.text, image_matches, TextType.IMAGE))
+                continue
+        ret.append(old_node)
+    return ret
+
+def split_node_matches_helper(text, matches, text_type):
+    ret = []
+    # Get first match
+    match_text, url = matches[0]
+    # Split on match
+    match = f"[{match_text}]({url})"
+    if text_type == TextType.IMAGE:
+        match = "!" + match
+    before, rest = text.split(match, 1)
+    # Create text node before special node if needed
+    if before:
+        ret.append(TextNode(before, TextType.TEXT))
+    # Create special node
+    ret.append(TextNode(match_text, text_type, url))
+    # Call recursively if there's more matches for special nodes
+    if len(matches) > 1:
+        ret.extend(split_node_matches_helper(rest, matches[1:], text_type))
+        return ret
+    # Otherwise make a text node if needed
+    elif rest:
+        ret.append(TextNode(rest, TextType.TEXT))
+        return ret
+    else:
+        return ret
